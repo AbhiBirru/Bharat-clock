08d3f857c45774a65d4ac2b246e24245750234b4
AbhiBirru <abhileshbirru7@gmail.com>
2024-06-16 19:33:46 +0530

    Added my project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import ClockHeading from './components/ClockHeading'
+import Clockslogan from './components/Clockslogan'
+import Clocktime from './components/Clocktime'
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css'
+
+
+function App() {
+  
+  return (
+    <center>
+    <ClockHeading/>
+    <Clockslogan/>
+    <Clocktime></Clocktime>
+   </center>
+    )
+}
+
+export default App
---
+++ src/components/Clockslogan.jsx
@@ -0,0 +1,4 @@
+let Clockslogan =()=>{
+    return <p>This is the clock that show the time in bharat all time</p>
+    }
+    export default Clockslogan;
